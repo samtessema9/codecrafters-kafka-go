19cdd2f1919bc8d0003ba467c806522fa62ff369
Sam Tessema <samtessema@Sams-MacBook-Air.local>
2025-01-22 22:48:54 -0800

    0 out message_size

--- app/server.go
+++ app/server.go
@@ -34,7 +34,7 @@ func main() {
 	var buf []byte
 	bufWithWriter := bytes.NewBuffer(buf)
 
-	messageSize := uint32(4)
+	messageSize := uint32(0)
 	correlationId := uint32(7)
 	
 	// write the message_size and correlation_id to the buffer in BigEndian binary format
