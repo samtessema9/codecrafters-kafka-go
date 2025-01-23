951e57c2e5d2bf3f5075700842f82642cd13995f
Sam Tessema <samtessema@Sams-MacBook-Air.local>
2025-01-22 23:40:07 -0800

    add ability to handle multiple connections and multiple messages from a connection

--- app/server.go
+++ app/server.go
@@ -23,27 +23,39 @@ func main() {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer l.Close()
+	fmt.Println("Server is listening on port 9092...")
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		
+		go handleConn(conn)
 	}
+}
+
+func handleConn(conn net.Conn) {
 	defer conn.Close()
-
-	// read and ignore clients message
-	readBuf := []byte{}
-	_, _ = conn.Read(readBuf)
-
-	// create write buffer
-	writeBuf := new(bytes.Buffer)
-
-	messageSize := uint32(0)
-	correlationId := uint32(7)
 	
-	// write the message_size and correlation_id to the buffer in BigEndian binary format
-	binary.Write(writeBuf, binary.BigEndian, messageSize)
-	binary.Write(writeBuf, binary.BigEndian, correlationId)
-
-	// respond to the client with the value stored in our buffer
-	conn.Write(writeBuf.Bytes())
+	for {
+		// read and ignore clients message
+		readBuf := []byte{}
+		_, _ = conn.Read(readBuf)
+	
+		// create write buffer
+		writeBuf := new(bytes.Buffer)
+	
+		messageSize := uint32(0)
+		correlationId := uint32(7)
+		
+		// write the message_size and correlation_id to the buffer in BigEndian binary format
+		binary.Write(writeBuf, binary.BigEndian, messageSize)
+		binary.Write(writeBuf, binary.BigEndian, correlationId)
+	
+		// respond to the client with the value stored in our buffer
+		conn.Write(writeBuf.Bytes())
+	}
 }
